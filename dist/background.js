0c9f165a2c023a6f83db45cbe13bb61af541b058
= <clement.traor@gmail.com>
2024-03-25 08:31:34 +0100

    1.0

--- dist/background.js
+++ dist/background.js
@@ -1,6 +1,6 @@
 document.body.addEventListener('keydown', (event) => {
     if (event.key === 'Enter'){
-        var searchString = document.getElementById('searchString').value;
+        var searchString = document.getElementById('searchInput').value;
         imgSearch(searchString);
         async function imgSearch(){
             const reponse = await fetch('https://api.pexels.com/v1/search?query=' + searchString +'&per_page=1&page=1',{
@@ -10,13 +10,18 @@ document.body.addEventListener('keydown', (event) => {
             });
             const data = await reponse.json();
             const photo = data.photos[0];
+            const color = data.photos[0].avg_color;
            const url = data.photos[0].src.original;
            const photographer = data.photos[0].photographer;
            const desc = data.photos[0].alt;
            const screen = document.getElementById('onescreen');
            screen.style.backgroundImage = `url(${url})`;
+           screen.style.backgroundImage.alt = desc;
            screen.style.backgroundSize = "cover";
           
+            changeColors(color);
+           
+            
            addPhotographer(photographer, desc);
             if(logo == 0){
               addPexelsLogo();
@@ -24,15 +29,31 @@ document.body.addEventListener('keydown', (event) => {
             }
             
            }
-        document.getElementById('searchString').value = '';
+        document.getElementById('searchInput').value = '';
+    }
+    function changeColors(color){
+      var colors = [color.substring(1, 3), color.substring(3,5), color.substring(5,7)];
+      var display = document.getElementById("display");
+      var lightness = 0;
+      colors.forEach(primary => {
+       lightness += parseInt(primary, 16);
+      });
+      if(lightness < 382.5){
+        display.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+        display.style.color = "black";
+        display.style.textShadow = "none";
+      }
+      else{
+        display.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        display.style.color = "white";
+        display.style.textShadow = "1px 0 #052a8f, -1px 0 #052a8f, 0 1px #052a8f, 0 -1px #052a8f, 1px 1px #052a8f, -1px -1px #052a8f, 1px -1px #052a8f, -1px 1px #052a8f;";
+      }
     }
     function addPexelsLogo(){
      document.getElementById('logo').innerHTML += '<img src="./img/pexels.png">'
     }
-    function addPhotographer(name, desc){
+    function addPhotographer(name){
       const photographe = document.getElementById('photographe');
       photographe.innerText = `Photographe: ${name}`;
-      const photodesc = document.getElementById('photoDesc');
-      photodesc.innerText = desc;
     }
 })
--- dist/main.js
+++ dist/main.js
@@ -2,7 +2,7 @@ var apikey = '113db2a1990e94ec2a19f906191136fb';
 var logo = 0;
 document.body.addEventListener('keydown', (event) => {
     if (event.key === 'Enter'){
-        var searchString = document.getElementById('searchString').value;
+        var searchString = document.getElementById('searchInput').value;
         getWeather(searchString);
     }
 })
@@ -37,21 +37,66 @@ function displayWeather(description, icon, temp, feels_like, min, max, wind_spee
             const desc = document.getElementById('desc');
             const minimum = document.getElementById('minimum');
             const maximum = document.getElementById('maximum');
-          
             const weatherIcon = document.getElementById('weatherIcon');
+            
             city.innerText = cityName;
-           temperature.innerHTML =` <span class="text-6xl">${Math.round(temp)}<span>°`;
-           ressenti.innerHTML = `ressenti:<pre> </pre><span class="text-xl"> ${Math.round(feels_like)}<span>°`;
-           desc.innerHTML = `<span class="text-2xl">${description}</span>`;
-           minimum.innerHTML = `<span class="text-xl">${Math.round(min)}</span>° / <span class="text-xl">${Math.round(max)}</span>° `;
-           wind.innerHTML = `<img src="./img/wind_speed.svg" class="w-16 h-32">  ${Math.round(wind_speed*1.85)} km/h`;
-           weatherIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png">`
+           temperature.innerHTML =` ${Math.round(temp)}°`;
+           ressenti.innerHTML = `Ressenti:<pre> </pre> ${Math.round(feels_like)}°`;
+           desc.innerHTML = `${description}`;
+           minimum.innerHTML = `${Math.round(min)}° / ${Math.round(max)}° `;
+           wind.innerHTML = `<img src="./img/wind_speed.svg" class=" w-16 h-32 md:w-32 md:h-64">  ${Math.round(wind_speed*1.85)} km/h`;
+           weatherIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@4x.png">`
+
+           clothesIcons(feels_like)
+           document.getElementById("display").style.opacity = "100";
 }
 function showBackground(searchString){
     var url = `https://api.unsplash.com/search/photos?page=1&query=${searchString}`
     document.body.style.backgroundImage = `<img src="${url}" />`    
     };
+function clothesIcons(ressenti){
+    let tenueHiver = ["t-shirt", "/", "shirt", "sweater", "coat", "pants" ]
+    let tenueEte = ["t-shirt", "/", "shirt", "shorts" ]
+    let tenuePrintemps = ["t-shirt", "/", "shirt", "jacket", "shorts",, "/", "pants" ]
+    let tenuePrintemps2 = ["t-shirt", "/", "shirt", "sweater", "pants"]
+    let tenueAutomne = ["t-shirt", "/", "shirt", "sweater", "jacket", "pants"]
+    let tenues = []
+    var habits = document.getElementsByClassName("habits")[0];
+    habits.style.opacity = 0.9;
+    
+    if(ressenti< 15 && ressenti>9){
+        tenues.push(tenueAutomne)
+    }
+    if(ressenti<10 ){
+        tenues.push(tenueHiver)
+    }
+    if(ressenti >15 && ressenti < 25){
+        tenues.push(tenuePrintemps, tenuePrintemps2)
+    }
+    if(ressenti> 20 ){
+    tenues.push(tenueEte)
+    }
+    showClothes(tenues);
+    
+    
+}
+function showClothes(array){
+    var habits = document.getElementsByClassName("habits")[0];
+    habits.innerHTML = "";
+    habits.style.opacity = 0.9;
 
+  for(i = 0; i< array.length; i++){
+    habits.innerHTML += `<div id='tenue${i}' class="tenue flex h-1/3 md:h-auto items-center wrap w-fit "><span class="tenueTitle absolute md:relative opacity-0 md:opacity-100"> Tenue ${i+1}</span></div>`
+    array[i].forEach(clothe =>{
+        if(clothe != "/"){
+            document.getElementById(`tenue${i}`).innerHTML += `<img src="./img/${clothe}.png" class="clotheIcon w-14 md:w-20 mx-1"/>`
+        }
+        else{
+        document.getElementById(`tenue${i}`).innerHTML += `<span class="separation">/</span>`;
+        }
+    })
+  }
+}
 function showError(){
  let error = document.getElementById('error');
  error.classList.remove("opacity-0");
@@ -62,4 +107,7 @@ let error = document.getElementById('error');
     setTimeout(() => {
         error.classList.add('opacity-0');
     },5000)
+}
+function deleteTitle(){
+    document.getElementById("title").innerText = "";
 }
